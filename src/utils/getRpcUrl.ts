3f72b7462531cdccb279fb438f2a8d82fe4009b1
RetroDefi BSC <82356199+RetroDefi@users.noreply.github.com>
2021-07-24 11:41:19 -0400

    try

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -12,9 +12,9 @@ const REACT_APP_NODE_3 = "https://bsc-dataseed.binance.org";
 // Array of available nodes to connect to
 export const nodes = [REACT_APP_NODE_1, REACT_APP_NODE_2, REACT_APP_NODE_3];
 
-const getNodeUrl = () => {
+function getNodeUrl() {
   const randomIndex = random(0, nodes.length - 1);
   return nodes[randomIndex];
-};
+}
 
 export default getNodeUrl;
--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -8,9 +8,9 @@ const httpProvider = new Web3.providers.HttpProvider(RPC_URL, {
 } as HttpProviderOptions);
 const web3NoAccount = new Web3(httpProvider);
 
-const getWeb3NoAccount = () => {
+function getWeb3NoAccount() {
   return web3NoAccount;
-};
+}
 
 export { getWeb3NoAccount };
 export default web3NoAccount;
