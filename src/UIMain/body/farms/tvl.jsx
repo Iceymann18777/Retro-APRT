8b2e7fbf13050f893efda64b171d331f829a7828
RetroDefi BSC <82356199+RetroDefi@users.noreply.github.com>
2021-08-02 11:28:04 -0400

    up

--- src/UIMain/body/farms/tvl.jsx
+++ src/UIMain/body/farms/tvl.jsx
@@ -1,9 +1,15 @@
 import { Fragment, useState, useEffect } from "react";
 import CountUp from "react-countup";
 import Countdown from "react-countdown";
-import { getWeb3NoAccount } from "../../../utils/web3Global";
+import { Contract, Provider } from "ethcall";
+import { JsonRpcProvider } from "@ethersproject/providers";
+import getRpcUrl from "../../../utils/getRpcUrl";
+//import { getWeb3NoAccount } from "../../../utils/web3Global";
 //import { formatNumberHumanize } from "../../../utils/formatBalance";
 import { nativeFarmAbi } from "../../../Resources/lib/abi";
+
+const RPC_URL = getRpcUrl();
+const providerTVL = new JsonRpcProvider(RPC_URL);
 const farmAddress = "0x738600B15B2b6845d7Fe5B6C7Cb911332Fb89949";
 
 export default function Tvl() {
@@ -11,14 +17,20 @@ export default function Tvl() {
   var [timeLeft, setTimeLeft] = useState(5);
   var [text, setText] = useState("...");
 
-  const web3 = getWeb3NoAccount();
-  let pool = new web3.eth.Contract(nativeFarmAbi, farmAddress);
+  //const web3 = getWeb3NoAccount();
 
   const GetHarvestTimer = async () => {
+    let multiCall = new Provider();
+    await multiCall.init(providerTVL);
+    let pool = new Contract(farmAddress, nativeFarmAbi);
     try {
-      var currentBlock = await web3.eth.getBlockNumber();
-      let startBlockHarvest = await pool.methods.startBlockHarvest().call();
-      var startBlock = await pool.methods.startBlock().call();
+      var currentBlock = await providerTVL.getBlockNumber();
+      let startBlockHarvestCall = pool.startBlockHarvest();
+      var startBlockCall = pool.startBlock();
+      let [startBlockHarvest, startBlock] = await multiCall.all([
+        startBlockHarvestCall,
+        startBlockCall
+      ]);
       var startBlockTime = startBlock - currentBlock;
       var startBlockHarvestTime = startBlockHarvest - currentBlock;
       if (startBlockTime > 0) {
